e9243305975cb71a605e3b506e31f92bee15ff9d
JacobePro <jrmateo03@gmail.com>
2022-02-02 10:16:45 -0500

    Finished

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -59,7 +59,7 @@ public class TechJobs {
 
                 // What is their search term?
                 System.out.println("\nSearch term:");
-                String searchTerm = in.nextLine();
+                String searchTerm = in.nextLine().toUpperCase();
 
                 if (searchField.equals("all")) {
                     printJobs(JobData.findByValue(searchTerm));
@@ -120,6 +120,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() > 0) {
+            for (HashMap<String, String> aJob : someJobs) {
+                System.out.println("\n*****");
+                for(Map.Entry<String, String> data: aJob.entrySet()) {
+                    System.out.println(data.getKey() + ": " + data.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
+        else {
+            System.out.print("No Results");
+        }
     }
 }
